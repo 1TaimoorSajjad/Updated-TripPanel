df177eb600afddb778430671b5c0e5bcdba54192
Taimoors7 <taimoorsajjad980@gmail.com>
2023-06-10 23:18:33 +0500

    Creating the sidnav and the main body

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TripdetailsComponent } from './Components/tripdetails/tripdetails.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : "UserForm",
+    component : TripdetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
