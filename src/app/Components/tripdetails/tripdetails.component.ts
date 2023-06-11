4879ef9a842ea9bab97e291e977380ecce7d0615
Taimoors7 <taimoorsajjad980@gmail.com>
2023-06-12 01:16:26 +0500

    Added the functionality to populate the data from the database

--- src/app/Components/tripdetails/tripdetails.component.ts
+++ src/app/Components/tripdetails/tripdetails.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tripdetails',
@@ -8,11 +7,25 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./tripdetails.component.css']
 })
 export class TripdetailsComponent implements OnInit {
+  userForms: any[] = [];
 
-  constructor(){}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-   
+    this.fetchUserForms();
   }
 
+  fetchUserForms() {
+    this.http.get<any>('https://final-build-f2a86-default-rtdb.firebaseio.com/users.json')
+      .subscribe(
+        (response) => {
+          if (response) {
+            this.userForms = Object.values(response);
+          }
+        },
+        (error) => {
+          console.log('Error fetching data:', error);
+        }
+      );
+  }
 }
